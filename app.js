cc3676d0499164596b59c1535b134dd7682238f5
NimraFatima <farimanim16@gmail.com>
2024-03-22 00:34:46 -0600

    firebase

--- app.js
+++ app.js
@@ -27,19 +27,58 @@
 //       errorMessage.textContent = error.message;
 //     });
 // });
-function signup() {
-  let firstName = document.getElementById("first-name").value;
-  let lasttName = document.getElementById("last-name").value;
+
+
+// function signup() {
+//   let firstName = document.getElementById("first-name").value;
+//   let lasttName = document.getElementById("last-name").value;
+//   let signupemail = document.getElementById("email").value;
+//   let signuppassword = document.getElementById("password").value;
+//   let signuprepeatpassword = document.getElementById("repeat-password").value;
+//   if (signuppassword === signuprepeatpassword && firstName !== "" && lasttName !== "" && signupemail !== "" && signuppassword !== "" && signuprepeatpassword !== "") {
+//     window.location.assign("login.html")
+//   } else if(signuppassword !== signuprepeatpassword){
+//     alert("password not match");
+
+//   }else{
+//     alert("please fill input");
+//   }
+
+// }
+
+
+//fire base
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyAw8VHs9Kt_BEI5xv2oCby0Elh79NjosEo",
+  authDomain: "auth-fc41d.firebaseapp.com",
+  projectId: "auth-fc41d",
+  storageBucket: "auth-fc41d.appspot.com",
+  messagingSenderId: "166758835349",
+  appId: "1:166758835349:web:9a0f13cfac57400f07a79c"
+});
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+
+//sign up function
+
+const signup=()=>{
   let signupemail = document.getElementById("email").value;
   let signuppassword = document.getElementById("password").value;
-  let signuprepeatpassword = document.getElementById("repeat-password").value;
-  if (signuppassword === signuprepeatpassword && firstName !== "" && lasttName !== "" && signupemail !== "" && signuppassword !== "" && signuprepeatpassword !== "") {
-    window.location.assign("login.html")
-  } else if(signuppassword !== signuprepeatpassword){
-    alert("password not match");
+  console.log(signupemail,signuppassword)
+  //firebase code
+  firebase.auth().createUserWithEmailAndPassword(signupemail, signuppassword)
 
-  }else{
-    alert("please fill input");
-  }
-
+  .then((result) => {
+    // Signed in 
+   document.write("you are signed up")
+    console.log(result)
+    // ...
+  })
+  .catch((error) => {
+    console.log(error.code)
+    console.log(error.message)
+    // ..
+  });
 }
+
